4f4436e0db5e4ac25083419433fa06a0205e5d46
Packstudio <packstudio@naver.com>
2026-01-21 19:22:18 +0900

    Move folder

---
+++ src/server.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+
+// const app = express();
+
+import express from "express";
+
+const app = express();
+
+const handleListening = () => console.log("Server listening on port: 4000");
+
+app.listen(4000, handleListening);
